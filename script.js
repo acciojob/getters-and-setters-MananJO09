831e5d7e255d63710d41ba172a91a666a0a2e9aa
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-07-18 22:53:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //complete this code
-class Person {}
+class Person {
+	this.name=name;
+	this.age=age;
 
-class Student extends Person {}
+	  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+}
+
+class Student extends Person {
+	study() {
+    console.log(`${this.name} is studying.`);
+  }
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
